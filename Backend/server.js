2d65734988a1432bb1fbea343faf803b1f23780b
letaunahn <letuananh28072002@gmail.com>
2024-05-07 18:11:28 +0700

    Create run server

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+
+// app config
+const app = express();
+const port = 3000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+// api endpoint
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
